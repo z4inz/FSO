58f8be72b8a5d5f9f6d39a5503dfe43a558d42bf
z4inz <zainzafk@gmail.com>
2024-02-28 18:43:15 +0000

    part 3

---
+++ part3/README.md
@@ -0,0 +1,1 @@
+https://fso-part3-z.fly.dev/
---
+++ part3/index.js
@@ -0,0 +1,121 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.use(express.static('dist'))
+
+const cors = require('cors')
+
+app.use(cors())
+
+const morgan = require('morgan')
+morgan.token('body', (request, response) => {
+	return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const Person = require('./models/person')
+
+app.get('/info/', (request, response) => {
+	Person.countDocuments({})
+		.then( count => {
+			response.send(`<p>Phonebook has info for ${count} people</p><p>${(new Date()).toString()}</p>`)
+		})
+		.catch(error => next(error))
+})
+
+app.get('/api/persons/', (request, response) => {
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+	Person.findById(request.params.id)
+		.then(person => {
+			if (person) {
+				response.json(person)
+			}
+			else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+	Person.findByIdAndDelete(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
+})
+
+app.post('/api/persons/', (request, response, next) => {
+	const body = request.body
+
+	if (body.name === undefined) {
+		return response.status(400).json({ error: 'name missing' })
+	}
+
+	const person = new Person({
+		name: body.name,
+		number: body.number
+	})
+
+	person.save()
+		.then(savedPerson => {
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
+})
+
+
+app.put('/api/persons/:id', (request, response, next) => {
+	const { name, number } = request.body
+
+	Person.findByIdAndUpdate(
+		request.params.id,
+		{ name, number },
+		{ new: true, runValidators: true, context: 'query' }
+	)
+		.then(updatedPerson => {
+			if (updatedPerson) {
+				response.json(updatedPerson)
+			}
+			else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+	else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
+
+	next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
