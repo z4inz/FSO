a999f50c90afa00006b615c20813d5c20f3ec639
z4inz <zainzafk@gmail.com>
2024-03-10 23:17:46 +0000

    exercise 4.12

--- part4/app.js
+++ part4/app.js
@@ -27,5 +27,6 @@ app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/tests/bloglist_api.test.js
+++ part4/tests/bloglist_api.test.js
@@ -31,6 +31,74 @@ test('unique identifier property of blog post is named "id"', async () => {
   assert.strictEqual(response.body[0]._id, undefined)
 })
 
+test('successfully creates a new blog post', async () => {
+  const newBlog = {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+})
+
+test('blog likes defaults to 0 if missing', async () => {
+  const newBlog = {
+    title: "Test blog post",
+    author: "Zain",
+    url: "http://zain.com",
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  const blogWithZeroLikes = blogsAtEnd.find(blog => blog.title === "Test blog post")
+  assert.strictEqual(blogWithZeroLikes.likes, 0)
+})
+
+test('title missing', async () => {
+  const newBlog = {
+    author: "Zain",
+    url: "http://zain.com",
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
+test('url missing', async () => {
+  const newBlog = {
+    title: "Test blog post",
+    author: "Zain",
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
